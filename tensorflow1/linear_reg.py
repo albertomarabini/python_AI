277e5bc65d86b622c96de09fd43f0567e9509337
albertomarabini <albertomarabini@gmail.com>
2023-12-02 14:27:32 +0100

    a

--- tensorflow1/linear_reg.py
+++ tensorflow1/linear_reg.py
@@ -1,37 +1,18 @@
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 import numpy as np
 import pandas as pd
-import statsmodels.formula.api as sm
-from matplotlib import cm
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+from six.moves import urllib
 
-csv = pd.read_csv('http://www-bcf.usc.edu/~gareth/ISL/Advertising.csv', index_col=0)
-model = sm.ols(formula='Sales ~ TV + Radio', data = csv)
-fit = model.fit()
+import tensorflow.compat.v2.feature_column as fc
 
-fit.summary()
+import tensorflow as tf
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+# Load dataset.
+dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv') # training data
+dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv') # testing data
+y_train = dftrain.pop('survived')
+y_eval = dfeval.pop('survived')
 
-x_surf = np.arange(0, 350, 20)                # generate a mesh
-y_surf = np.arange(0, 60, 4)
-x_surf, y_surf = np.meshgrid(x_surf, y_surf)
-
-exog = pd.core.frame.DataFrame({'TV': x_surf.ravel(), 'Radio': y_surf.ravel()})
-out = fit.predict(exog = exog)
-ax.plot_surface(x_surf, y_surf,
-                out.reshape(x_surf.shape),
-                rstride=1,
-                cstride=1,
-                color='None',
-                alpha = 0.4)
-
-ax.scatter(csv['TV'], csv['Radio'], csv['Sales'],
-           c='blue',
-           marker='o',
-           alpha=1)
-
-ax.set_xlabel('TV')
-ax.set_ylabel('Radio')
-ax.set_zlabel('Sales')
